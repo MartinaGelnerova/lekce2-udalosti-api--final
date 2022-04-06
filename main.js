837b1fb483a78320dd1178da00ba67b0152e4f10
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-06 14:05:02 +0200

    refaktoring funkcí tlačítek k textu v odstavci

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-// tady je místo pro náš program
-
 /**
  * Funkce přidá nebo odebere na prvku třídu, která se stará o červenou barvu písma
- * 
- * @param {string} elementId 
  */
-function prepniZcervenani(elementId) {
-    document.getElementById(elementId).classList.toggle('cerveny-text');
-}
+let odstavec = document.getElementById('js-paragraph');
+document.getElementById('prepniZcervenani').addEventListener('click', () => {
+  odstavec.classList.toggle('cerveny-text');
+});
 
 /**
  * Funkce zvětší/změnší písmo v elementu o definovanou velikost
- * 
- * @param {string} elementSelector 
- * @param {int} fontSizeChange 
- */
-function zvetsitPismo(elementSelector, fontSizeChange) {
-    let element = document.querySelector(elementSelector);
-    let currentFontSize = parseInt(element.style.fontSize);
-    element.style.fontSize = (currentFontSize + fontSizeChange) + 'px';
-}
+*/
+document.getElementById('zvetsitPismo').addEventListener('click', () => {
+  let currentFontSize = parseInt(odstavec.style.fontSize);
+  let fontSizeChange = document.getElementById('zvetsitPismo').dataset.fontSizeChange;
+  odstavec.style.fontSize = (currentFontSize + parseInt(fontSizeChange)) + 'px';
+});
 
 /*--------------*/
 /* AUDIO FUNKCE */
